0eb0f6a46cb5b5f4a98618b37da5ab72733d56bb
Jesus Alexis <ingjesusalexis@gmail.com>
2021-12-27 14:56:56 -0400

    added .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TuitsModule } from './modules/tuits/tuits.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [TuitsModule, TypeOrmModule.forRoot({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "admin",
-    password: "my-weak-password",
-    database: "postgres",
-    autoLoadEntities: true,
-    synchronize: true,
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TuitsModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: "localhost",
+      port: 5432,
+      username: "admin",
+      password: "my-weak-password",
+      database: "postgres",
+      autoLoadEntities: true,
+      synchronize: true,
 
-  }), UsersModule],
+    }),
+    UsersModule],
 
 })
-export class AppModule { }
+export class AppModule {
+  static port: number;
+  constructor(private readonly configService: ConfigService) {
+    AppModule.port = +this.configService.get("PORT")
+  }
+}

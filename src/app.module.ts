738c6c1fc8f4162db6ac2a0c8e3065504612910e
Jesus Alexis <ingjesusalexis@gmail.com>
2021-12-27 21:18:00 -0400

    config database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { TuitsModule } from './modules/tuits/tuits.module';
 import { UsersModule } from './modules/users/users.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
     TuitsModule,
-    TypeOrmModule.forRoot({
-      type: "postgres",
-      host: "localhost",
-      port: 5432,
-      username: "admin",
-      password: "my-weak-password",
-      database: "postgres",
-      autoLoadEntities: true,
-      synchronize: true,
-
-    }),
-    UsersModule],
+    UsersModule,
+  ],
 
 })
 export class AppModule {
